d56a6aea2584c909185209ed3271b6bf2a6473cd
Dimitar <dim_kolev2002@abv.bg>
2019-08-31 12:21:46 +0300

    Start project. Add Data Models

---
+++ ELibrary.My.Data/ConfigurationData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ELibrary.My.Data
+{
+    public static class ConfigurationData
+    {
+        public const string ConnectionString = "Server=.\\SQLEXPRESS; Database=ELibrary; Integrated Security=true";
+    }
+}
---
+++ ELibrary.My.View.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ELibrary.My.View.Models
+{
+    public class Class1
+    {
+    }
+}
